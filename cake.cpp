a587d28b1dbb2ab82bb98c27a80c2ff2eb910586
AAYUSH2091 <aayushanand407@gmail.com>
2025-09-13 18:06:38 +0530

    solved_more

---
+++ cake.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+
+    solve();
+
+    return 0;
+}
+
+//2^k+1 total cakes
+//c and v revieced 2^k intitally
+// x and 2^k+1  - x
